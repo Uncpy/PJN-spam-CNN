c5e23bb788595913f99fc6430dd0b9573a481187
Kuba Janicki <jakub.janicki@icloud.com>
2017-10-28 20:56:34 +0200

    Fix imports in clean.py
    Also remove @staticmethod decorator

--- Helpers/clean.py
+++ Helpers/clean.py
@@ -1,6 +1,8 @@
-import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import string
 
-@staticmethod
 def clean(line):
     tokens = word_tokenize(line)
     tokens = [w.lower() for w in tokens]
