2eadabc6dade704efb8093868115cf15e880b733
Kuba Janicki <jakub.janicki@icloud.com>
2017-10-28 20:36:56 +0200

    Add static cleaning method as a Helper
    This method will be used for cleaning up passed lines.
    
    May actually remove stemming in the future as it may cause accuracy issues.

---
+++ Helpers/clean.py
@@ -0,0 +1,19 @@
+import re
+
+@staticmethod
+def clean(line):
+    tokens = word_tokenize(line)
+    tokens = [w.lower() for w in tokens]
+    
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+    
+    words = [word for word in stripped if word.isalpha()]
+    
+    stop_words = set(stopwords.words('english'))
+    words = [w for w in words if not w in stop_words]
+    
+    porter = PorterStemmer()
+    stemmed = [porter.stem(word) for word in tokens]
+
+    return ' '.join(words)
